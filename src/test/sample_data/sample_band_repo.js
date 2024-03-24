a776d844bf57698ce01494a6f2d3378a9ddc3866
Rick Vermeil <rickv85@gmail.com>
2024-03-24 16:33:30 -0600

    Create sample_band_repo test data

---
+++ src/test/sample_data/sample_band_repo.js
@@ -0,0 +1,46 @@
+export const sampleBandRepo = {
+  bands: [
+    {
+      band_name: "Global Groove",
+      members: [
+        {
+          name: "You",
+          income: 2600,
+        },
+        {
+          name: "Carlos",
+          income: 2400,
+        },
+        {
+          name: "Deepak",
+          income: 2700,
+        },
+        {
+          name: "Elena",
+          income: 2500,
+        },
+      ],
+    },
+    {
+      band_name: "Melody Makers",
+      members: [
+        {
+          name: "You",
+          income: 400,
+        },
+        {
+          name: "Liam",
+          income: 500,
+        },
+        {
+          name: "Aisha",
+          income: 600,
+        },
+        {
+          name: "Raj",
+          income: 700,
+        },
+      ],
+    },
+  ],
+};
