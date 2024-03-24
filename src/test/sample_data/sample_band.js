85faf4a738019b7f5bcfdb7aec69b37916c3b7a8
Rick Vermeil <rickv85@gmail.com>
2024-03-24 15:50:14 -0600

    Create sample_band test data

---
+++ src/test/sample_data/sample_band.js
@@ -0,0 +1,21 @@
+export const bandData = {
+  band_name: "Harmony Fusion",
+  members: [
+    {
+      name: "You",
+      income: 300
+    },
+    {
+      name: "Chen",
+      income: 150
+    },
+    {
+      name: "Yuki",
+      income: 200
+    },
+    {
+      name: "Oliver",
+      income: 100
+    }
+  ]
+}
