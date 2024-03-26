babe1e45ed8c55d169326465ecb306b7024843c4
Rick Vermeil <rickv85@gmail.com>
2024-03-26 11:16:48 -0600

    Scale up html font sizes on all break points

--- src/components/TotalIncome/TotalIncome.jsx
+++ src/components/TotalIncome/TotalIncome.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default function TotalIncome({ total }) {
   return (
     <div className="total-income-div">
-      <h2 className="total-income">Total Income for 2023-24</h2>
+      <h2 className="total-income">Total Income for 2023-24:</h2>
       <h2 className="total-income-amt">{`$${total ? total : 0}`}</h2>
     </div>
   );
