00b3c6deeffb4cab1608248525ec9f01faf24f8b
Rick Vermeil <rickv85@gmail.com>
2024-03-26 10:37:09 -0600

    Style income display header

--- src/components/IncomeDisplay/IncomeDisplay.jsx
+++ src/components/IncomeDisplay/IncomeDisplay.jsx
@@ -8,16 +8,17 @@ export default function IncomeDisplay({ selectedBand }) {
       return "0";
     }
   };
+
   const bandIncomeDisplay = () => {
     if (selectedBand) {
       return (
-        <div>
+        <header className="income-display-header">
           <h3>{selectedBand.bandName}</h3>
-          <div>
-            <h4>Total Band Income</h4>
-            <h4>{bandTotalIncome()}</h4>
+          <div className="total-band-income-div">
+            <h4>Total Band Income:</h4>
+            <p>${bandTotalIncome()}</p>
           </div>
-        </div>
+        </header>
       );
     } else {
       return null;
