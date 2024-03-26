68f5b1f9e4e2e8c5b78b9ffdccf27e0b7f13f867
Rick Vermeil <rickv85@gmail.com>
2024-03-26 14:10:05 -0600

    Style member view inputs

--- src/components/MemberView/MemberView.jsx
+++ src/components/MemberView/MemberView.jsx
@@ -1,11 +1,16 @@
 import { object } from "prop-types";
 import { sortByNameUserFirst } from "../../utils/utils";
+import { useState } from "react";
 
 export default function MemberView({ bandRepo }) {
+  const [searchValue, setSearchValue] = useState("");
+  const [filterSelection, setFilterSelection] = useState("all");
+
   const createAllMemberIncomeDisplay = () => {
     const sortedMemArr = sortByNameUserFirst(
       bandRepo.createYearlyTotalIncomeByMember()
     );
+
     const memberElements = sortedMemArr.map((mem, i) => {
       let memClass = "mem-inc";
       if (mem.income >= 600) {
@@ -25,8 +30,22 @@ export default function MemberView({ bandRepo }) {
     <>
       <header className="income-display-header">
         <div className="mem-view-input-div">
-          <input></input>
-          <select></select>
+          <input
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            type="text"
+            placeholder="Search by name"
+            className="mem-view-input"
+          />
+          <select
+            value={filterSelection}
+            onChange={(e) => setFilterSelection(e.target.value)}
+            className="mem-view-select"
+          >
+            <option value={"all"}>All Members</option>
+            <option value={"over600"}>{`Members >= $600`}</option>
+            <option value={"under600"}>{`Members < $600`}</option>
+          </select>
         </div>
         <h3>All Members</h3>
         <h4>{`My Income: $${bandRepo.calcTotalUserIncome()}`}</h4>
