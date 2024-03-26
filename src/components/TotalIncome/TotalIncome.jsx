d8fa001131e9c407d9a2fac5fd9043480179b500
Rick Vermeil <rickv85@gmail.com>
2024-03-26 08:54:20 -0600

    Add default total of 0 to TotalIncome display

--- src/components/TotalIncome/TotalIncome.jsx
+++ src/components/TotalIncome/TotalIncome.jsx
@@ -4,11 +4,11 @@ export default function TotalIncome({ total }) {
   return (
     <div className="total-income-div">
       <h2 className="total-income">Total Income for 2023-24</h2>
-      <h2 className="total-income-amt">{`$${total ? total : "______"}`}</h2>
+      <h2 className="total-income-amt">{`$${total ? total : 0}`}</h2>
     </div>
   );
 }
 
 TotalIncome.propTypes = {
-  total: PropTypes.string,
+  total: PropTypes.number,
 };
