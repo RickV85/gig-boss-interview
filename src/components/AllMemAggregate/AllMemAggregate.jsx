10f967fa003e44aa8c36393aa63b9e07fab8e4a4
Rick Vermeil <rickv85@gmail.com>
2024-03-27 12:19:35 -0600

    Create AllMemAggregate logic and display

--- src/components/AllMemAggregate/AllMemAggregate.jsx
+++ src/components/AllMemAggregate/AllMemAggregate.jsx
@@ -1,10 +1,38 @@
 import { array } from "prop-types";
 
 export default function AllMemAggregate({ memArr }) {
+  const createDispElementsFromMemArr = () => {
+    if (memArr?.length) {
+      const memberElements = memArr.map((mem, i) => {
+        let memClass = "mem-agg-income";
+        // Create styled elements to highlight users with >= $600 income
+        if (mem.income >= 600) {
+          memClass += " over-600";
+        }
+        return (
+          <div className="mem-agg-income-div" key={`memAggInc-${i}`}>
+            <p className={memClass}>{`${mem.name}`}</p>
+            <p className={memClass}>{`$${mem.income}`}</p>
+          </div>
+        );
+      });
+      return memberElements;
+    }
+  };
+
   return (
     <section className="all-mem-agg-section">
+      <div className="all-mem-agg-container">
+        <header className="all-mem-agg-header">
+          <h2>Musician</h2>
+          <h2>Income</h2>
+        </header>
+        <div className="all-mem-agg-display">
+          {createDispElementsFromMemArr()}
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 AllMemAggregate.propTypes = {
