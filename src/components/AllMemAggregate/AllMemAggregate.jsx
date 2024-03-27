3c4144ff36e4d54ef1214e30f4f6e36c34c4165d
Rick Vermeil <rickv85@gmail.com>
2024-03-27 11:20:52 -0600

    Style aggregate component sections

--- src/components/AllMemAggregate/AllMemAggregate.jsx
+++ src/components/AllMemAggregate/AllMemAggregate.jsx
@@ -1,7 +1,6 @@
 import { array } from "prop-types";
 
 export default function AllMemAggregate({ memArr }) {
-  console.log(memArr)
   return (
     <section className="all-mem-agg-section">
     </section>
